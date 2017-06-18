b0f5c34ab50c7a0e9f1f8875dbf70e96b9c29ab6
larry brader <lbrader@microsoft.com>
2017-06-18 13:47:35 -0700

    refactor parameters\sharepoint and onpremise control flow

--- sharepoint/sharepoint-2016/Deploy-ReferenceArchitecture.ps1
+++ sharepoint/sharepoint-2016/Deploy-ReferenceArchitecture.ps1
@@ -1,5 +1,5 @@
 #
-# Deploy_ReferenceArchitecture.ps1
+# Deploy_ReferenceArchitecture.ps1 for SharePoint 2016 with OnPremise 
 #
 param(
     [Parameter(Mandatory = $true)]
@@ -7,7 +7,7 @@ param(
     [Parameter(Mandatory = $true)]
     $Location,
     [Parameter(Mandatory = $true)]
-    [ValidateSet("Infrastructure", "Security", "Workload")]
+    [ValidateSet("All", "Onpremise","Infrastructure", "CreateVpn","ConnectVPN", "Workload","Security")]
     $Mode
 )
 
@@ -29,6 +29,9 @@ Write-Host
 Write-Host "Using $templateRootUriString to locate templates"
 Write-Host
 
+#
+# Building Block definitions
+#
 $templateRootUri = New-Object System.Uri -ArgumentList @($templateRootUriString)
 
 $loadBalancerTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, "templates/buildingBlocks/loadBalancer-backend-n-vm/azuredeploy.json")
@@ -38,44 +41,63 @@ $virtualMachineTemplate = New-Object System.Uri -ArgumentList @($templateRootUri
 $virtualMachineExtensionsTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, "templates/buildingBlocks/virtualMachine-extensions/azuredeploy.json")
 $networkSecurityGroupTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, "templates/buildingBlocks/networkSecurityGroups/azuredeploy.json")
 
-# Azure ADDS Parameter Files
-$domainControllersParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\adds\ad.parameters.json")
-$virtualNetworkDNSParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\adds\virtualNetwork-adds-dns.parameters.json")
-$addAddsDomainControllerExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\adds\add-adds-domain-controller.parameters.json")
-$createAddsDomainControllerForestExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\adds\create-adds-forest-extension.parameters.json")
+
+# ###########################
+# SharePoint 2016 definitions 
+#
+
+# SharePoint Azure ADDS Parameter Files
+$domainControllersParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\adds\ad.parameters.json")
+$virtualNetworkDNSParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\adds\virtualNetwork-adds-dns.parameters.json")
+$addAddsDomainControllerExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\adds\add-adds-domain-controller.parameters.json")
+$createAddsDomainControllerForestExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\adds\create-adds-forest-extension.parameters.json")
 
 # SQL Always On Parameter Files
-$sqlParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sql.parameters.json")
-$fswParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\fsw.parameters.json")
-$sqlPrepareAOExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sql-iaas-ao-extensions.parameters.json")
-$sqlConfigureAOExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sql-configure-ao-extension.parameters.json")
+$sqlParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\sql.parameters.json")
+$fswParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\fsw.parameters.json")
+$sqlPrepareAOExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\sql-iaas-ao-extensions.parameters.json")
+$sqlConfigureAOExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\sql-configure-ao-extension.parameters.json")
 
 # Infrastructure And Workload Parameters Files
-$virtualNetworkParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\virtualNetwork.parameters.json")
-$virtualNetworkGatewayParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\virtualNetworkGateway.parameters.json")
-$managementParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\virtualMachines-mgmt.parameters.json")
+$virtualNetworkParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\virtualNetwork.parameters.json")
+$virtualNetworkGatewayParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\virtualNetworkGateway.parameters.json")
+$managementParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\virtualMachines-mgmt.parameters.json")
 
-$appVirtualMachineParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\app.parameters.json")
-$webLoadBalancerParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\web.parameters.json")
-$dchVirtualMachineParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\dch.parameters.json")
-$srchVirtualMachineParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\srch.parameters.json")
-$createFarmApp1ExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\spt-create-farm-app1-ext.parameters.json")
-$configFarmDCH1ExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\spt-config-farm-dch1-ext.parameters.json")
-$configFarmWFE1SRCH1ExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\spt-config-farm-wfe1-srch1-ext.parameters.json")
-$configFarmWfe2App2ExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\spt-config-farm-wfe2-app2-ext.parameters.json")
-$configFarmDch2Srch2ExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\spt-config-farm-dch2-srch2-ext.parameters.json")
-$addArecordExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\add-dns-arecord.parameters.json")
-$networkSecurityGroupParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\networkSecurityGroups.parameters.json")
+$appVirtualMachineParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\app.parameters.json")
+$webLoadBalancerParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\web.parameters.json")
+$dchVirtualMachineParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\dch.parameters.json")
+$srchVirtualMachineParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\srch.parameters.json")
+$createFarmApp1ExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\spt-create-farm-app1-ext.parameters.json")
+$configFarmDCH1ExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\spt-config-farm-dch1-ext.parameters.json")
+$configFarmWFE1SRCH1ExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\spt-config-farm-wfe1-srch1-ext.parameters.json")
+$configFarmWfe2App2ExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\spt-config-farm-wfe2-app2-ext.parameters.json")
+$configFarmDch2Srch2ExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\spt-config-farm-dch2-srch2-ext.parameters.json")
+$addArecordExtensionParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\add-dns-arecord.parameters.json")
+$networkSecurityGroupParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\sharepoint\networkSecurityGroups.parameters.json")
+
+# ########################
+# OnPremise definitions
+#
 
 
-
+# ########################
+# Resource Groups definitions
+#
 $infrastructureResourceGroupName = "ra-sp2016-network-rg"
 $workloadResourceGroupName = "ra-sp2016-workload-rg"
 
 # Login to Azure and select your subscription
 Login-AzureRmAccount -SubscriptionId $SubscriptionId | Out-Null
 
-if ($Mode -eq "Infrastructure")
+if ($Mode -eq "Onpremise" -Or $Mode -eq "All") 
+{
+
+
+
+
+}
+
+if ($Mode -eq "Infrastructure" -Or $Mode -eq "All")
 {
     $infrastructureResourceGroup = New-AzureRmResourceGroup -Name $infrastructureResourceGroupName -Location $Location
     Write-Host "Creating virtual network..."
@@ -132,8 +154,30 @@ if ($Mode -eq "Infrastructure")
         -ResourceGroupName $infrastructureResourceGroup.ResourceGroupName `
         -TemplateUri $virtualMachineExtensionsTemplate.AbsoluteUri -TemplateParameterFile $sqlConfigureAOExtensionParametersFile
 }
-elseif ($Mode -eq "Workload")
+if ($Mode -eq "CreateVPN" -Or $Mode -eq "All") 
 {
+    Write-Host "CreateVPN Section ---------------------------------------------------"
+    Write-Host "Add CreateVPN Section"
+
+
+
+}
+if ($Mode -eq "ConnectVPN" -Or $Mode -eq "All") 
+{
+    Write-Host "ConnectVPN Section ---------------------------------------------------"
+    Write-Host "Add ConnectVPN Section"
+
+
+
+
+}
+
+if ($Mode -eq "Workload" -Or $Mode -eq "All")
+{
+    Write-Host "SharePoint 2016 Workload Section ---------------------------------------------------"
+    Write-Host "Add CreateVPN Section"
+
+
     Write-Host "Creating workload resource group..."
    $workloadResourceGroup = New-AzureRmResourceGroup -Name $workloadResourceGroupName -Location $Location
 
@@ -187,9 +231,11 @@ elseif ($Mode -eq "Workload")
         -ResourceGroupName $workloadResourceGroup.ResourceGroupName -TemplateUri $virtualMachineExtensionsTemplate.AbsoluteUri `
         -TemplateParameterFile $addArecordExtensionParametersFile        
 }
-elseif ($Mode -eq "Security")
+
+if ($Mode -eq "Security" -Or $Mode -eq "All")
 {
-    # Deploy DMZs
+    Write-Host "SharePoint 2016 Security Section ---------------------------------------------------\n"
+
     $infrastructureResourceGroup = Get-AzureRmResourceGroup -Name $infrastructureResourceGroupName 
 
     Write-Host "Deploying NSGs..."
@@ -197,3 +243,5 @@ elseif ($Mode -eq "Security")
         -TemplateUri $networkSecurityGroupTemplate.AbsoluteUri -TemplateParameterFile $networkSecurityGroupParametersFile
 
 }
+
+Write-Host "Deployment of SharePoint 2016 with OnPremise network complete!"
