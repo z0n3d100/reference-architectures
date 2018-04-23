751915508809d0010e19adfda93ebffb0564151e
Pedro Wood <mail_pedro@yahoo.com.ar>
2018-04-23 11:05:41 -0300

    domain trust
    domain trust
    
    domain trust

---
+++ identity/adds-forest/domain-trust.ps1
@@ -0,0 +1,24 @@
+param(
+    [string]$strRemoteDomain = "contoso.com",
+    [Parameter(Mandatory=$true)][string]$strRemoteAdmin,
+    [Parameter(Mandatory=$true)][securestring]$strRemoteAdminPassword
+)
+
+$remoteContext = New-Object -TypeName "System.DirectoryServices.ActiveDirectory.DirectoryContext" -ArgumentList @( "Domain", $strRemoteDomain, $strRemoteAdmin, $strRemoteAdminPassword)
+try {
+        $remoteDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($remoteContext)
+        #Write-Host "GetDomain: Succeeded for domain $($remoteDomain)"
+    }
+catch {
+        Write-Warning "GetDomain: Failed:`n`tError: $($($_.Exception).Message)"
+    }
+Write-Host "Connected to domain: $($remoteDomain.Name)"
+$localDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain() 
+Write-Host "Connected to Local domain: $($localDomain.Name)"
+try {
+        $localDomain.CreateTrustRelationship($remoteDomain,"Inbound")
+        Write-Host "CreateTrustRelationship: Succeeded for domain $($remoteDomain)"
+    }
+catch {
+        Write-Warning "CreateTrustRelationship: Failed for domain $($remoteDomain)`n`tError: $($($_.Exception).Message)"
+    }
