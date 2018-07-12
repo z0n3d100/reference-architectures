dd0d70b6bc518e5bb1e33516eef62c3a188926d7
Pedro Wood <mail_pedro@yahoo.com.ar>
2018-07-12 13:06:37 -0300

    add missing file

---
+++ sharepoint/sharepoint-2016/add-dns.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module DNSServer
+
+Add-DnsServerResourceRecordA -Name "Portal" -ZoneName "contoso.local" -AllowUpdateAny -IPv4Address "10.0.1.100" -TimeToLive 01:00:00 -ComputerName "AD1.contoso.local"
+Add-DnsServerResourceRecordA -Name "OneDrive" -ZoneName "contoso.local" -AllowUpdateAny -IPv4Address "10.0.1.100" -TimeToLive 01:00:00 -ComputerName "AD1.contoso.local"
