d4ad01f4c1c5862fcc589f883bbcb707822ea0f3
Luis Gizirian <v-lugizi@microsoft.com>
2018-12-13 15:46:59 -0300

    Remove commented code. Re-package.

--- identity/adfs-v2/adfs-nodes.ps1
+++ identity/adfs-v2/adfs-nodes.ps1
@@ -115,15 +115,6 @@ Configuration AddADFSNode
             DependsOn = "[cADFSNode]AddADFSNode"
         }
 
-        # Service DrsService
-        # {
-        #     Name = "drs"
-        #     Ensure = "Present"
-        #     State = "Running"
-        #     StartupType = "Automatic"
-        #     DependsOn = "[cADFSNode]AddADFSNode"
-        # }
-
         $ServiceAccountName = $DomainCreds.UserName;
         
         cADFSDeviceRegistration cADFSDeviceRegistration
@@ -135,7 +126,7 @@ Configuration AddADFSNode
             ServiceAccountName = $ServiceAccountName
             RegistrationQuota = 10
             MaximumRegistrationInactivityPeriod = 90
-            DependsOn = "[Service]AdfsService"#,"[Service]DrsService"
+            DependsOn = "[Service]AdfsService"
             PsDscRunAsCredential = $DomainCreds
         }
     }
