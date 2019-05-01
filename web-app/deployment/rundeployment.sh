043ab7ea7f8683048f9e552aeda795d2ac9adb9b
cfarre <cfarre@microsoft.com>
2019-04-30 20:12:01 -0700

    cosmosdb automation

--- web-app/deployment/rundeployment.sh
+++ web-app/deployment/rundeployment.sh
@@ -34,8 +34,14 @@ connstring=`az sql db show-connection-string --server $SQLSERVERNAME --name $SQL
 connstring=$(echo $connstring | sed "s/<username>/${SQLADMINUSER}/g")
 connstring=$(echo $connstring | sed "s/<password>/${SQLADMINPASSWORD}/g")
 
- az sql server firewall-rule create -g $RGNAME -s $SQLSERVERNAME  -n azureservices --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+az sql server firewall-rule create -g $RGNAME -s $SQLSERVERNAME  -n azureservices --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
 
- sqlcmd -S tcp:${SQLSERVERNAME}.database.windows.net,1433 -d votingdb -U $SQLADMINUSER -P $SQLADMINPASSWORD -N -l 30 -Q "CREATE TABLE Counts(ID INT NOT NULL IDENTITY PRIMARY KEY, Candidate VARCHAR(32) NOT NULL, Count INT)"
+sqlcmd -S tcp:${SQLSERVERNAME}.database.windows.net,1433 -d votingdb -U $SQLADMINUSER -P $SQLADMINPASSWORD -N -l 30 -Q "CREATE TABLE Counts(ID INT NOT NULL IDENTITY PRIMARY KEY, Candidate VARCHAR(32) NOT NULL, Count INT)"
  
- az group deployment create --resource-group $RGNAME --template-uri ${DEPLOYMENT}webappdeploy.json --parameters VotingWeb_name=${DNSNAME} SqlConnectionString="${connstring}" certData=${certdata} certPassword=${CERTPASS}
+az group deployment create --resource-group $RGNAME --template-uri ${DEPLOYMENT}webappdeploy.json --parameters VotingWeb_name=${DNSNAME} SqlConnectionString="${connstring}" certData=${certdata} certPassword=${CERTPASS}
+
+cosmosacc=`az cosmosdb list -g ${RGNAME} | jq -r .[0].name`
+
+az cosmosdb database create -d cacheDB -n ${cosmosacc} -g ${RGNAME}
+
+az cosmosdb collection create --name ${cosmosacc} -c cacheContainer -g ${RGNAME} --db-name cacheDB --partition-key-path '/MessageType'
